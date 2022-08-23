cecb0f888907f5d84dd71e0598d86ecd65bff709
JKM0722 <108040612+JKM0722@users.noreply.github.com>
2022-08-23 20:16:20 +0900

    3

--- renmin/js/renmin.js
+++ renmin/js/renmin.js
@@ -1,4 +1,12 @@
 $(()=>{
+    $(document).ready(function() {
+        $('#fullpage').fullpage({
+            //options here
+            licenseKey: 'OPEN-SOURCE-GPLV3-LICENSE',
+            autoScrolling:true,
+            scrollHorizontally: true
+        });
+    });
 
     var menuButton = document.querySelector('.menu-button');
     var openMenu = function () {
