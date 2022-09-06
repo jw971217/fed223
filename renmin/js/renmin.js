56f633309375be096e7992becb97632ec8b530f2
jw971217 <jw971217@naver.com>
2022-09-06 22:38:42 +0900

    asdf
    asdf

--- renmin/js/renmin.js
+++ renmin/js/renmin.js
@@ -38,3 +38,14 @@ $(()=>{
     });
 
 });
+
+// text click and copy
+function copy(that){
+    var inp =document.createElement('input');
+    document.body.appendChild(inp)
+    inp.value =that.textContent
+    inp.select();
+    document.execCommand('copy',false);
+    inp.remove();
+    alert("Copied")
+    }
