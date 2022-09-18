03c839c45b96c6261d62ef1b57d5c9eea340555c
jw971217 <jw971217@naver.com>
2022-09-18 11:12:46 +0900

    asdf
    asdf

--- renmin/js/mobile.js
+++ renmin/js/mobile.js
@@ -1,3 +1,4 @@
+// swiper
 $(()=>{
 
     var swiper = new Swiper(".mySwiper", {
@@ -18,3 +19,30 @@ $(()=>{
         },
     });
 });
+
+
+// text click and copy
+function copy(that){
+    var inp =document.createElement('input');
+    document.body.appendChild(inp)
+    inp.value =that.textContent
+    inp.select();
+    document.execCommand('copy',false);
+    inp.remove();
+    alert("Copied")
+    }
+
+// date
+$(document).ready(function(){
+    var now = new Date();
+    let datee = ".datee"
+
+    var year=now.getFullYear();//연도
+    $(datee).eq(0).text(year+"."); 
+
+    var month=now.getMonth()+1;//월
+    $(datee).eq(1).text(month+".");
+
+    var date=now.getDate();//일
+    $(datee).eq(2).text(date);
+  });
