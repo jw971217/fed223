c60f4330421b385e6608c35643a15dbb92a71512
jw971217 <108041529+jw971217@users.noreply.github.com>
2022-11-28 17:55:55 +0900

    asdf
    asdf

--- renmin/js/renmin.js
+++ renmin/js/renmin.js
@@ -73,7 +73,7 @@ $(document).ready(function(){
         
         // 1024 이하인지 if문으로 확인
         if (width_size <= 1024) {
-            alert('현재 브라우저 크기가 너무 작습니다. 자동으로 모바일 화면으로 넘어갑니다.');
+            // alert('현재 브라우저 크기가 너무 작습니다. 자동으로 모바일 화면으로 넘어갑니다.');
             $(location).attr("href", "https://jw971217.github.io/fed223/renmin/renmin_mobile.html");
         }
     });
